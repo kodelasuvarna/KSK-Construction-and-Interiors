99ecbe9e4923b5baa0e72394a9d55b05aa12096a
Patternscompany <pandureddypatterns@gmail.com>
2025-01-29 13:31:26 +0530

    pp

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,6 +227,37 @@ jQuery(document).ready(function ($) {
           },
         },
       });
+    $(".gallery-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>',
+        ],
+        responsiveClass: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          576: {
+            items: 1,
+          },
+          768: {
+            items: 1,
+          },
+          992: {
+            items: 1,
+          },
+          1200: {
+            items: 3,
+          },
+        },
+      });
 
 // =========================================
 // Scroll Up
